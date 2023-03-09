cc6c441a858b4a6930704ed2e983c5c8076bca6d
suyogster <suyogshrestha.tech@gmail.com>
2023-03-09 16:10:43 +0545

    Different Tokens deployment logic added

--- TokenFactory/src/main/java/io/tokenfactory/score/TokenFactory.java
+++ TokenFactory/src/main/java/io/tokenfactory/score/TokenFactory.java
@@ -1,19 +1,15 @@
 package io.tokenfactory.score;
 
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonObject;
 import io.tokenfactory.score.dbs.ContractDB;
-import io.tokenfactory.score.utils.ArrayDBUtils;
 import io.tokenfactory.score.utils.IterableDictDB;
-import score.Address;
-import score.ArrayDB;
-import score.BranchDB;
-import score.VarDB;
+import score.*;
 import score.annotation.EventLog;
 import score.annotation.External;
 import score.annotation.Payable;
 
 import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.List;
 
 import static io.tokenfactory.score.Constant.*;
@@ -28,38 +24,38 @@ public class TokenFactory {
     private final IterableDictDB<BigInteger, ContractDB> deploymentDetail = new IterableDictDB<>(DEPLOYMENT_DETAIL, ContractDB.class, BigInteger.class, false);
     private final BranchDB<Address, ArrayDB<ContractDB>> deployerDetail = newBranchDB(CONTRACT_DEPLOYER, ContractDB.class);
 
-    public TokenFactory(Address admin, Address treasury, BigInteger deployFee){
-        if(this.admin.get() == null){
+    public TokenFactory(Address admin, Address treasury, BigInteger deployFee) {
+        if (this.admin.get() == null) {
             this.admin.set(admin);
             this.treasury.set(treasury);
             this.deployFee.set(deployFee);
         }
     }
 
+    @External(readonly = true)
+    public Address getAdmin() {
+        return this.admin.get();
+    }
+
     @External
-    public void setAdmin(Address admin){
+    public void setAdmin(Address admin) {
         ownerOnly();
         this.admin.set(admin);
     }
 
     @External(readonly = true)
-    public Address getAdmin(){
-        return this.admin.get();
-    }
-
-    @External
-    public void setTreasury(Address treasury){
-        ownerOnly();
-        this.treasury.set(treasury);
-    }
-
-    @External(readonly = true)
-    public Address getTreasury(){
+    public Address getTreasury() {
         return this.treasury.get();
     }
 
     @External
-    public void setContractContent(String name, byte[] content){
+    public void setTreasury(Address treasury) {
+        ownerOnly();
+        this.treasury.set(treasury);
+    }
+
+    @External
+    public void setContractContent(String name, byte[] content) {
         adminOnly();
         require(availableContracts.contains(name), Message.Not.validContract());
         require(!this.content.keys().contains(name), Message.duplicateContract());
@@ -67,32 +63,30 @@ public class TokenFactory {
     }
 
     @External(readonly = true)
-    public List<String> getAvailableContractContent(){
+    public List<String> getAvailableContractContent() {
         return this.content.keys();
     }
 
     @External(readonly = true)
-    public byte[] getContractContent(String key){
+    public byte[] getContractContent(String key) {
         return this.content.get(key);
     }
 
     @External(readonly = true)
-    public BigInteger getDeployFee(){
+    public BigInteger getDeployFee() {
         return this.deployFee.get();
     }
 
     @External
     @Payable
-    public void deployContract(String key, Address deployer){
+    public void deployContract(String key, Address deployer, byte[] _data) {
         require(!deployer.equals(ZERO_ADDRESS), Message.zeroAddress());
         require(getValue().equals(this.getDeployFee()), Message.paymentMismatch());
         require(this.content.keys().contains(key), Message.Not.deployed());
 
-        var currentSize = BigInteger.valueOf(this.deploymentDetail.keys().size() + 1);
-        var content = this.content.get(key);
-
-        Address contract = deploy(content, this.getAdmin(), this.getTreasury(), this.getDeployFee());
-        call(CHAIN_ADDRESS, "setScoreOwner",contract, deployer);
+        BigInteger currentSize = BigInteger.valueOf(this.deploymentDetail.keys().size() + 1);
+        byte[] content = this.content.get(key);
+        Address contract = deployContract(key, content, _data);
 
         ContractDB contractDB = new ContractDB(currentSize, key, deployer, BigInteger.valueOf(getBlockTimestamp()), contract);
         this.deploymentDetail.set(currentSize, contractDB);
@@ -100,30 +94,49 @@ public class TokenFactory {
         this.ContractDeployed(deployer, key);
     }
 
+    Address deployContract(String key, byte[] content, byte[] _data) {
+        JsonObject data = unpackAndFetchObject(_data);
+        switch (key) {
+            case "IRC2":
+                return deploy(content, data.get("name").asString(), data.get("symbol").asString(), data.get("decimal").asInt());
+            case "IRC3":
+                return deploy(content, data.get("param1"));
+            default:
+                throw new UserRevertedException("Invalid Key");
+        }
+    }
+
+    JsonObject unpackAndFetchObject(byte[] _data) {
+        String unpackedData = new String(_data);
+        return Json.parse(unpackedData).asObject();
+    }
+
+
     @External(readonly = true)
-    public ContractDB getDeployedContracts(Address deployer){
+    public ContractDB getDeployedContracts(Address deployer) {
         return this.deployerDetail.at(deployer).get(0);
     }
 
     @External(readonly = true)
-    public int deployCount(){
+    public int deployCount() {
         return this.deploymentDetail.keys().size();
     }
 
     @External
-    public void transferToTreasury(BigInteger amount){
+    public void transferToTreasury(BigInteger amount) {
         this.ownerOnly();
         transfer(getTreasury(), amount);
     }
 
     @EventLog(indexed = 2)
-    public void ContractDeployed(Address deployer, String name){}
+    public void ContractDeployed(Address deployer, String name) {
+    }
 
-    void ownerOnly(){
+    void ownerOnly() {
         require(getCaller().equals(getOwner()), Message.Not.owner());
     }
 
-    void adminOnly(){
+    void adminOnly() {
         require(getCaller().equals(getAdmin()), Message.Not.admin());
     }
 }
