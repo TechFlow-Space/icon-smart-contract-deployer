999c8ffaff731139eb80b58bfdc3a4070503cb5b
naneey <nanneychan03@gmail.com>
2023-03-31 11:12:26 +0545

    irc2 test covered

--- GenericIRC2/src/test/java/io/contractdeployer/generics/irc2/IRC2Test.java
+++ GenericIRC2/src/test/java/io/contractdeployer/generics/irc2/IRC2Test.java
@@ -4,7 +4,7 @@ import com.iconloop.score.test.Account;
 import com.iconloop.score.test.Score;
 import com.iconloop.score.test.ServiceManager;
 import com.iconloop.score.test.TestBase;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
@@ -12,32 +12,28 @@ import score.Address;
 
 import java.math.BigInteger;
 
-import static io.contractdeployer.generics.irc2.TestHelper.expectErrorMessage;
 import static java.math.BigInteger.TEN;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 
 public class IRC2Test extends TestBase {
 
     protected static final Address ZERO_ADDRESS = new Address(new byte[Address.LENGTH]);
     private static final String name = "IRC2Token";
     private static final String symbol = "IRC2";
-    private static final int decimals = 18;
-    private static final BigInteger initialSupply = BigInteger.valueOf(1000);
-
-    private static BigInteger totalSupply = initialSupply.multiply(TEN.pow(decimals));
+    private static final BigInteger decimals = BigInteger.valueOf(18);
     private static final ServiceManager sm = getServiceManager();
     private static final Account owner = sm.createAccount();
-    private static final Account user = sm.createAccount();
-    private static final Account minter = sm.createAccount();
+    private static final Account alice = sm.createAccount();
+    private static final Account bob = sm.createAccount();
     private static Score ircScore;
     private static IRC2 tokenSpy;
 
     @BeforeEach
     public void setup() throws Exception {
-        ircScore = sm.deploy(owner, IRC2.class,
-                name, symbol, BigInteger.valueOf(decimals),minter.getAddress());
-        ircScore.invoke(minter,"mint",owner.getAddress(),totalSupply);
+        ircScore = sm.deploy(owner, IRC2.class, name, symbol, decimals,owner.getAddress());
 
         // setup spy object against the ircScore object
         tokenSpy = (IRC2) spy(ircScore.getInstance());
@@ -56,77 +52,144 @@ public class IRC2Test extends TestBase {
 
     @Test
     void decimals(){
-        assertEquals(ircScore.call("decimals"),BigInteger.valueOf(decimals));
+        assertEquals(ircScore.call("decimals"),decimals);
     }
 
     @Test
     void totalSupply(){
-        assertEquals(ircScore.call("totalSupply"),totalSupply);
+        assertEquals(ircScore.call("totalSupply"),BigInteger.ZERO);
     }
 
     @Test
-    void balanceOf(){
-        assertEquals(ircScore.call("balanceOf",owner.getAddress()),totalSupply);
-        assertEquals(ircScore.call("balanceOf",user.getAddress()),BigInteger.ZERO);
-    }
+    void setMinter_not_by_owner(){
+        Executable call = () -> ircScore.invoke(alice,"setMinter", alice.getAddress());
+        expectErrorMessage(call, "IRC2 :: Only owner can perform this action.");
 
-    @Test
-    void transfer(){
-        byte[] data=new byte[0];
-
-        Executable call = () -> ircScore.invoke(owner,"transfer", owner.getAddress(),BigInteger.ZERO,data);
-        expectErrorMessage(call, Message.Not.self());
-
-        call = () -> ircScore.invoke(owner,"transfer", user.getAddress(),BigInteger.ZERO,data);
-        expectErrorMessage(call, Message.greaterThanZero("_value"));
-
-        call = () -> ircScore.invoke(user,"transfer", owner.getAddress(), BigInteger.TEN,data);
-        expectErrorMessage(call, Message.Not.enoughBalance());
-
-        ircScore.invoke(owner,"transfer", user.getAddress(), BigInteger.TEN,data);
-        assertEquals(ircScore.call("balanceOf",user.getAddress()),BigInteger.TEN);
-        assertEquals(ircScore.call("balanceOf",owner.getAddress()),totalSupply.subtract(BigInteger.TEN));
+        verify(tokenSpy,never()).TokenMinterUpdated(alice.getAddress());
     }
 
     @Test
     void setMinter(){
-        Executable call = () -> ircScore.invoke(user,"setMinter", user.getAddress());
-        expectErrorMessage(call, Message.Not.owner());
 
-        ircScore.invoke(owner,"setMinter", user.getAddress());
-        assertEquals(ircScore.call("getMinter"),user.getAddress());
+        ircScore.invoke(owner,"setMinter", alice.getAddress());
+        assertEquals(ircScore.call("getMinter"), alice.getAddress());
+
+        verify(tokenSpy).TokenMinterUpdated(alice.getAddress());
+    }
+
+    @Test
+    void mint_negative_value(){
+        BigInteger negativeAmount = BigInteger.valueOf(100).negate();
+
+        Executable call = () -> ircScore.invoke(owner,"mint",alice.getAddress(),negativeAmount);
+        expectErrorMessage(call, "amount needs to be positive");
+    }
+
+    @Test
+    void mint_to_zero_address(){
+        BigInteger amount = BigInteger.valueOf(100).multiply(ICX);
+
+        Executable call = () -> ircScore.invoke(owner,"mint",ZERO_ADDRESS,amount);
+        expectErrorMessage(call, "Owner address cannot be zero address");
+    }
+
+    @Test
+    void mint_more_than_cap(){
+        BigInteger amount = BigInteger.valueOf(1001).multiply(ICX);
+
+        ircScore.invoke(owner,"mint",alice.getAddress(),amount);
+
     }
 
     @Test
     void mint(){
-        int val=10000;
-        Account admin=sm.createAccount();
-        ircScore.invoke(owner,"setMinter", admin.getAddress());
+        BigInteger amount = BigInteger.valueOf(100).multiply(ICX);
 
-        Executable call = () -> ircScore.invoke(owner,"mint", owner.getAddress(),BigInteger.valueOf(val));
-        expectErrorMessage(call, Message.Not.minter());
+        // mint to alice
+        ircScore.invoke(owner,"mint", alice.getAddress(),amount);
 
-        call = () -> ircScore.invoke(admin,"mint", ZERO_ADDRESS,BigInteger.valueOf(val));
-        expectErrorMessage(call, Message.Found.zeroAddr("Owner"));
+        assertEquals(ircScore.call("totalSupply"),amount);
+        assertEquals(ircScore.call("balanceOf",alice.getAddress()),amount);
 
-        call = () -> ircScore.invoke(admin,"mint", user.getAddress(),BigInteger.ZERO);
-        expectErrorMessage(call, Message.greaterThanZero("Amount"));
+        // mint to bob
+        ircScore.invoke(owner,"mint", bob.getAddress(),amount);
 
-        ircScore.invoke(admin,"mint", owner.getAddress(),BigInteger.valueOf(val));
-        assertEquals(ircScore.call("totalSupply"),totalSupply.add(BigInteger.valueOf(val)));
-        assertEquals(ircScore.call("balanceOf",owner.getAddress()),totalSupply.add(BigInteger.valueOf(val)));
+        assertEquals(ircScore.call("totalSupply"),amount.add(amount));
+        assertEquals(ircScore.call("balanceOf",bob.getAddress()),amount);
+
+        // verification
+        verify(tokenSpy).Mint(owner.getAddress(),alice.getAddress(),amount);
+        verify(tokenSpy).Mint(owner.getAddress(),bob.getAddress(),amount);
+        verify(tokenSpy).Transfer(ZERO_ADDRESS,alice.getAddress(),amount,"mint".getBytes());
+        verify(tokenSpy).Transfer(ZERO_ADDRESS,bob.getAddress(),amount,"mint".getBytes());
+    }
+
+    @Test
+    void mint_after_ownership_transfer(){
+        BigInteger amount = BigInteger.valueOf(100).multiply(ICX);
+
+        assertEquals(ircScore.call("getMinter"),owner.getAddress());
+
+        // minter access to alice
+        ircScore.invoke(owner,"setMinter",alice.getAddress());
+
+        assertEquals(ircScore.call("getMinter"),alice.getAddress());
+
+        //  mint allowed by owner and minter
+        ircScore.invoke(alice,"mint",bob.getAddress(),amount);
+        ircScore.invoke(owner,"mint",bob.getAddress(),amount);
+
+        verify(tokenSpy).Mint(owner.getAddress(),bob.getAddress(),amount);
+        verify(tokenSpy).Mint(alice.getAddress(),bob.getAddress(),amount);
+
+    }
+
+    @Test
+    void burn_negative_amount(){
+        BigInteger amount = BigInteger.valueOf(10).multiply(ICX);
+
+        // mint to alice
+        ircScore.invoke(owner,"mint", alice.getAddress(),amount);
+
+        Executable call = () -> ircScore.invoke(alice,"burn", amount.negate());
+        expectErrorMessage(call, "amount needs to be positive");
+
+    }
+
+    @Test
+    void burn_more_than_balance(){
+        BigInteger amount = BigInteger.valueOf(10).multiply(ICX);
+
+        // mint to alice
+        ircScore.invoke(owner,"mint", alice.getAddress(),amount.subtract(BigInteger.ONE));
+
+        Executable call = () -> ircScore.invoke(alice,"burn", amount);
+        expectErrorMessage(call, "Insufficient balance");
+
     }
 
     @Test
     void burn(){
-        int val=10000;
+        BigInteger amount = BigInteger.valueOf(100).multiply(ICX);
 
-        Executable call = () -> ircScore.invoke(owner,"burn", BigInteger.ZERO);
-        expectErrorMessage(call, Message.greaterThanZero("Amount"));
+        ircScore.invoke(owner,"mint",bob.getAddress(),amount);
 
-        ircScore.invoke(owner,"burn", BigInteger.valueOf(val));
-        assertEquals(ircScore.call("totalSupply"),totalSupply.subtract(BigInteger.valueOf(val)));
-        assertEquals(ircScore.call("balanceOf",owner.getAddress()),totalSupply.subtract(BigInteger.valueOf(val)));
+        assertEquals(ircScore.call("balanceOf",bob.getAddress()),amount);
+        assertEquals(ircScore.call("totalSupply"),amount);
+
+        BigInteger amountToBurn = TEN.multiply(ICX);
+        ircScore.invoke(bob,"burn", amountToBurn);
+
+        assertEquals(ircScore.call("balanceOf",bob.getAddress()),amount.subtract(amountToBurn));
+        assertEquals(ircScore.call("totalSupply"),amount.subtract(amountToBurn));
+
+        verify(tokenSpy).Burn(bob.getAddress(),amountToBurn);
+        verify(tokenSpy).Transfer(bob.getAddress(),ZERO_ADDRESS,amountToBurn,"burn".getBytes());
+    }
+
+    public static void expectErrorMessage(Executable contractCall, String errorMessage) {
+        AssertionError e = Assertions.assertThrows(AssertionError.class, contractCall);
+        assertEquals(errorMessage, e.getMessage());
     }
 
 }
