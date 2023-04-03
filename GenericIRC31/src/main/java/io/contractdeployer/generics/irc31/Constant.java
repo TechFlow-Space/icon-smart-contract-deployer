c3741a18fc2de0640077513f910d079430b78180
naneey <nanneychan03@gmail.com>
2023-04-03 11:23:08 +0545

    irc31 contract update

--- GenericIRC31/src/main/java/io/contractdeployer/generics/irc31/Constant.java
+++ GenericIRC31/src/main/java/io/contractdeployer/generics/irc31/Constant.java
@@ -6,9 +6,6 @@ public class Constant {
     static final String NAME="name";
     static final String SYMBOL="symbol";
     static final String CAP="cap";
-    static final String TOKEN_URI="token_uri";
-    static final String APPROVAL="approval";
-    static final String BALANCES="balances";
     static final String ADMIN="admin";
     static final String MAX_BATCH_MINT="max_batch_mint";
     static final String TOTAL_SUPPLY="total_supply";
--- GenericIRC31/src/main/java/io/contractdeployer/generics/irc31/IRC31.java
+++ GenericIRC31/src/main/java/io/contractdeployer/generics/irc31/IRC31.java
@@ -1,18 +1,31 @@
 package io.contractdeployer.generics.irc31;
 
+import com.iconloop.score.token.irc31.IRC31Basic;
+import io.contractdeployer.generics.irc31.exception.IRC31Exception;
 import score.Address;
-import score.ByteArrayObjectWriter;
 import score.Context;
-import score.DictDB;
-import score.annotation.EventLog;
+import score.VarDB;
 import score.annotation.External;
-import score.annotation.Optional;
 
 import java.math.BigInteger;
 
-import static io.contractdeployer.generics.irc31.Vars.*;
+import static io.contractdeployer.generics.irc31.Constant.ADMIN;
+import static io.contractdeployer.generics.irc31.Constant.CAP;
+import static io.contractdeployer.generics.irc31.Constant.MAX_BATCH_MINT;
+import static io.contractdeployer.generics.irc31.Constant.NAME;
+import static io.contractdeployer.generics.irc31.Constant.SYMBOL;
+import static io.contractdeployer.generics.irc31.Constant.TOTAL_SUPPLY;
 
-public class IRC31 implements InterfaceIRC31 {
+
+public class IRC31 extends IRC31Basic implements InterfaceIRC31{
+
+    public static final Address ZERO_ADDRESS = new Address(new byte[Address.LENGTH]);
+    public final VarDB<String> name = Context.newVarDB(NAME, String.class);
+    public final VarDB<String> symbol = Context.newVarDB(SYMBOL, String.class);
+    public final VarDB<BigInteger> cap = Context.newVarDB(CAP, BigInteger.class);
+    public final VarDB<BigInteger> maxBatchMintCount = Context.newVarDB(MAX_BATCH_MINT, BigInteger.class);
+    public final VarDB<BigInteger> totalSupply = Context.newVarDB(TOTAL_SUPPLY, BigInteger.class);
+    public final VarDB<Address> admin = Context.newVarDB(ADMIN, Address.class);
 
     public IRC31(String _name, String _symbol,BigInteger _cap,BigInteger _maxBatchMintCount) {
 
@@ -20,8 +33,8 @@ public class IRC31 implements InterfaceIRC31 {
             name.set(ensureNotEmpty(_name));
             symbol.set(ensureNotEmpty(_symbol));
 
-            Context.require(_cap.compareTo(BigInteger.ZERO)>0,Message.greaterThanZero("_cap"));
-            Context.require(_maxBatchMintCount.compareTo(BigInteger.ZERO)>0,Message.greaterThanZero("_maxBatchMintCount"));
+            Context.require(_cap.compareTo(BigInteger.ZERO)>0, IRC31Exception.lessThanZero());
+            Context.require(_maxBatchMintCount.compareTo(BigInteger.ZERO)>0, IRC31Exception.lessThanZero());
 
             name.set(_name);
             symbol.set(_symbol);
@@ -31,11 +44,7 @@ public class IRC31 implements InterfaceIRC31 {
         }
     }
 
-    private String ensureNotEmpty(String str) {
-        Context.require(str != null && !str.trim().isEmpty(), Message.empty("String"));
-        assert str != null;
-        return str.trim();
-    }
+
 
     @External(readonly=true)
     public String name() {
@@ -54,7 +63,7 @@ public class IRC31 implements InterfaceIRC31 {
 
     @External(readonly = true)
     public BigInteger getTotalSupply() {
-        return totalSupply.get();
+        return totalSupply.getOrDefault(BigInteger.ZERO);
     }
 
     @External(readonly = true)
@@ -69,119 +78,28 @@ public class IRC31 implements InterfaceIRC31 {
 
     @External
     public void setAdmin(Address adminAddress) {
-        this.ownerRequired();
+        this.onlyOwner();
         admin.set(adminAddress);
     }
 
-    @External(readonly=true)
-    public BigInteger balanceOf(Address _owner, BigInteger _id) {
-        return balances.at(_id).getOrDefault(_owner, BigInteger.ZERO);
-    }
-
-    @External(readonly=true)
-    public BigInteger[] balanceOfBatch(Address[] _owners, BigInteger[] _ids) {
-        Context.require(_owners.length == _ids.length, Message.Not.sameSize("_owners","_ids"));
-
-        BigInteger[] balances = new BigInteger[_owners.length];
-        for (int i = 0; i < _owners.length; i++) {
-            balances[i] = balanceOf(_owners[i], _ids[i]);
-        }
-        return balances;
-    }
-
-    @External(readonly=true)
-    public String tokenURI(BigInteger _tokenId) {
-        return tokenURIs.get(_tokenId);
-    }
 
     @External
-    public void transferFrom(Address _from, Address _to, BigInteger _id, BigInteger _value, @Optional byte[] _data) {
-        final Address caller = Context.getCaller();
+    public void mint(Address _to, BigInteger _id, BigInteger _amount,String _uri) {
+        this.preMintConditions(_to,_amount);
+        _mintInternal(_to, _id, _amount,_uri);
 
-        Context.require(!_to.equals(ZERO_ADDRESS), Message.Found.zeroAddr("_to"));
-        Context.require(_from.equals(caller) || this.isApprovedForAll(_from, caller), Message.Not.operatorApproved());
-        Context.require(BigInteger.ZERO.compareTo(_value) <= 0 && _value.compareTo(balanceOf(_from, _id)) <= 0,
-                Message.Not.enoughBalance());
-
-        // Transfer funds
-        DictDB<Address, BigInteger> balance = balances.at(_id);
-        balance.set(_from, balanceOf(_from, _id).subtract(_value));
-        balance.set(_to, balanceOf(_to, _id).add(_value));
-
-        // Emit event
-        this.TransferSingle(caller, _from, _to, _id, _value);
-
-        if (_to.isContract()) {
-            // Call {@code onIRC31Received} if the recipient is a contract
-            Context.call(_to, "onIRC31Received", caller, _from, _id, _value, _data == null ? new byte[]{} : _data);
-        }
-    }
-
-    @External
-    public void transferFromBatch(Address _from, Address _to, BigInteger[] _ids, BigInteger[] _values, @Optional byte[] _data) {
-        final Address caller = Context.getCaller();
-
-        Context.require(!_to.equals(ZERO_ADDRESS), Message.Found.zeroAddr("_to"));
-        Context.require(_ids.length == _values.length, Message.Not.sameSize("_ids","_values"));
-        Context.require(_from.equals(caller) || this.isApprovedForAll(_from, caller), Message.Not.operatorApproved());
-
-        for (int i = 0; i < _ids.length; i++) {
-            BigInteger _id = _ids[i];
-            BigInteger _value = _values[i];
-
-            Context.require(_value.compareTo(BigInteger.ZERO) > 0, Message.greaterThanZero("_value"));
-
-            BigInteger balanceFrom = balanceOf(_from, _id);
-
-            Context.require(_value.compareTo(balanceFrom) <= 0, Message.Not.enoughBalance());
-
-            // Transfer funds
-            BigInteger balanceTo = balanceOf(_to, _id);
-            DictDB<Address, BigInteger> balance = balances.at(_id);
-            balance.set(_from, balanceFrom.subtract(_value));
-            balance.set(_to, balanceTo.add(_value));
-        }
-
-        // Emit event
-        this.TransferBatch(caller, _from, _to, rlpEncode(_ids), rlpEncode(_values));
-
-        if (_to.isContract()) {
-            // call {@code onIRC31BatchReceived} if the recipient is a contract
-            Context.call(_to, "onIRC31BatchReceived",
-                    caller, _from, _ids, _values, _data == null ? new byte[]{} : _data);
-        }
-    }
-
-    @External
-    public void setApprovalForAll(Address _operator, boolean _approved) {
-        final Address caller = Context.getOrigin();
-
-        operatorApproval.at(caller).set(_operator, _approved);
-        this.ApprovalForAll(caller, _operator, _approved);
-    }
-
-    @External(readonly=true)
-    public boolean isApprovedForAll(Address _owner, Address _operator) {
-        return operatorApproval.at(_owner).getOrDefault(_operator, false);
-    }
-
-    @External
-    public void mint(Address _owner, BigInteger _id, BigInteger _amount,String _uri) {
-        this.preMintConditions(_owner,_id,_amount);
-        _mintInternal(_owner, _id, _amount,_uri);
-
-        TransferSingle(_owner, ZERO_ADDRESS, _owner, _id, _amount);
+        TransferSingle(Context.getCaller(), ZERO_ADDRESS, _to, _id, _amount);
     }
 
     @External
     public void mintBatch(Address _owner, BigInteger[] _ids, BigInteger[] _amounts,String[] _uris) {
-        Context.require(_ids.length == _amounts.length, Message.Not.sameSize("_ids","_amounts"));
-        Context.require(_ids.length == _uris.length, Message.Not.sameSize("_ids","_uris"));
+        Context.require(_ids.length == _amounts.length, IRC31Exception.pairMismatch());
+        Context.require(_ids.length == _uris.length, IRC31Exception.pairMismatch());
 
         for (int i = 0; i < _ids.length; i++) {
             BigInteger id = _ids[i];
             BigInteger amount = _amounts[i];
-            this.preMintConditions(_owner,id,amount);
+            this.preMintConditions(_owner,amount);
             _mintInternal(_owner, id, amount,_uris[i]);
         }
 
@@ -199,7 +117,7 @@ public class IRC31 implements InterfaceIRC31 {
 
     @External
     public void burnBatch(Address _owner, BigInteger[] _ids, BigInteger[] _amounts) {
-        Context.require(_ids.length == _amounts.length, Message.Not.sameSize("_ids","_amounts"));
+        Context.require(_ids.length == _amounts.length, IRC31Exception.pairMismatch());
 
         for (int i = 0; i < _ids.length; i++) {
             BigInteger id = _ids[i];
@@ -211,75 +129,55 @@ public class IRC31 implements InterfaceIRC31 {
         TransferBatch(_owner, _owner, ZERO_ADDRESS, rlpEncode(_ids), rlpEncode(_amounts));
     }
 
-    protected static byte[] rlpEncode(BigInteger[] ids) {
-        Context.require(ids != null);
-
-        ByteArrayObjectWriter writer = Context.newByteArrayObjectWriter("RLPn");
-
-        writer.beginList(ids.length);
-        for (BigInteger v : ids) {
-            writer.write(v);
-        }
-        writer.end();
-
-        return writer.toByteArray();
-    }
-
-    private void _setTokenURI(BigInteger _id, String _uri) {
-        Context.require(_uri.length() > 0, Message.empty("URI"));
-        tokenURIs.set(_id, _uri);
-        this.URI(_id, _uri);
-    }
+    // ================================================
+    // Internal methods
+    // ================================================
 
     private void _mintInternal(Address owner, BigInteger id, BigInteger amount,String uri) {
-        BigInteger balance = balanceOf(owner, id);
-        balances.at(id).set(owner, balance.add(amount));
+
         totalSupply.set(getTotalSupply().add(amount));
         _setTokenURI(id,uri);
+
+        super._mint(owner, id, amount);
     }
 
     private void _burnInternal(Address owner, BigInteger id, BigInteger amount) {
-        BigInteger balance = balanceOf(owner, id);
-        balances.at(id).set(owner, balance.subtract(amount));
+
         totalSupply.set(getTotalSupply().subtract(amount));
+
+        super._burn(owner,id,amount);
     }
 
-    private void ownerRequired() {
-        Context.require(Context.getCaller().equals(Context.getOwner()), Message.Not.owner());
+    private void onlyOwner() {
+        Context.require(Context.getCaller().equals(Context.getOwner()), IRC31Exception.notOwner());
     }
 
-    private void adminRequired() {
+    private void onlyAdminOrOwner() {
         Context.require(Context.getCaller().equals(this.getAdmin()) || Context.getCaller().equals(Context.getOwner()),
-                Message.Not.admin());
+                IRC31Exception.notAdmin());
     }
 
-    private void preMintConditions(Address address, BigInteger id, BigInteger amount) {
-        adminRequired();
-        Context.require(!address.equals(ZERO_ADDRESS), Message.Found.zeroAddr("_owner"));
+    private void preMintConditions(Address address, BigInteger amount) {
+        onlyAdminOrOwner();
+        Context.require(!address.equals(ZERO_ADDRESS), IRC31Exception.zeroAddr());
         Context.require(amount.compareTo(BigInteger.ZERO) > 0 &&
-                amount.compareTo(getMaxBatchMintCount())<=0, Message.Exceeded.nftCountPerTxRange());
-        Context.require(getTotalSupply().add(amount).compareTo(getCap())<=0,Message.Exceeded.cap());
+                amount.compareTo(getMaxBatchMintCount())<=0, IRC31Exception.nftCountPerTxRange());
+        Context.require(getTotalSupply().add(amount).compareTo(getCap())<=0, IRC31Exception.capExceeded());
     }
 
     private void preBurnConditions(Address address, BigInteger id, BigInteger amount) {
-        final Address caller = Context.getOrigin();
-        Context.require(!address.equals(ZERO_ADDRESS), Message.Found.zeroAddr("_owner"));
-        Context.require(amount.compareTo(BigInteger.ZERO) > 0, Message.greaterThanZero("_amount"));
+        final Address caller = Context.getCaller();
+        Context.require(!address.equals(ZERO_ADDRESS), IRC31Exception.zeroAddr());
+        Context.require(amount.compareTo(BigInteger.ZERO) > 0, IRC31Exception.lessThanZero());
         Context.require(address.equals(caller) || this.isApprovedForAll(address, caller),
-                Message.Not.operatorApproved());
-        Context.require(balanceOf(address,id).compareTo(amount)>=0,Message.Not.enoughBalance());
+                IRC31Exception.notApproved());
+        Context.require(balanceOf(address,id).compareTo(amount)>=0, IRC31Exception.insufficientBalance());
     }
 
-    @EventLog(indexed=3)
-    public void TransferSingle(Address _operator, Address _from, Address _to, BigInteger _id, BigInteger _value) {}
-
-    @EventLog(indexed=3)
-    public void TransferBatch(Address _operator, Address _from, Address _to, byte[] _ids, byte[] _values) {}
-
-    @EventLog(indexed=2)
-    public void ApprovalForAll(Address _owner, Address _operator, boolean _approved) {}
-
-    @EventLog(indexed=1)
-    public void URI(BigInteger _id, String _value) {}
+    private String ensureNotEmpty(String str) {
+        Context.require(str != null && !str.trim().isEmpty(), IRC31Exception.empty());
+        assert str != null;
+        return str.trim();
+    }
 
 }
